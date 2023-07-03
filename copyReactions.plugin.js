2c53bdbe0407da1182edcd4bb38383758b00f15e
jericjan <cantilfrederick@gmail.com>
2023-07-03 21:00:13 +0800

    seems functional now

--- copyReactions.plugin.js
+++ copyReactions.plugin.js
@@ -8,12 +8,57 @@
 // var running = true;
 
 module.exports = (meta) => {
+  console.log("CopyReactions loading");
+
   let running = true;
-  let observer;
+  const observer = new MutationObserver(handleMutations);
+  let currentMsgId;
+  let listeners = [];
   // Do stuff in here before returning
+  // Callback function to handle the mutations
+  function handleMutations(mutations) {
+    mutations.forEach(function (mutation) {
+      mutation.addedNodes.forEach(function (node) {
+        if (node.matches && node.matches("[class^=chatContent]")) {
+          addRightClickListener(node);
+        } else {
+          //console.log("Ignored right click")
+        }
+      });
+    });
+  }
+
+  function NestedParent(element, selector) {
+    return element.closest(selector);
+  }
+
+  function addRightClickListener(node) {
+    console.log("Adding right click listener. context:", this);
+    // The element with the selector '[class^=chatContent]' has been added
+    console.log("Element with selector '[class^=chatContent]' added:", node);
+
+    node.addEventListener("contextmenu", handleRightClick);
+    listeners.push({
+      eventType: "contextmenu",
+      listener: handleRightClick,
+    });
+  }
+
+  function handleRightClick(event) {
+    var targetElement = event.target;
+    var parentElement = NestedParent(targetElement, "[id^=chat-messages]");
+    if (parentElement != null) {
+      var parentElemParts = parentElement.id.split("-");
+      var msgId = parentElemParts[parentElemParts.length - 1];
+      currentMsgId = msgId;
+      console.log("Right clicked! Id is ", msgId);
+    }
+  }
+
   return {
     start: () => {
-      let currentMsgId;
+      console.log("CopyReactions started");
+
       var list = [];
       var reactMenuCount = 0;
       var scrollerSelector =
@@ -45,312 +90,48 @@ module.exports = (meta) => {
           const value = reactions[key];
           const userCount = Object.keys(value.users).length;
           const storedEmoteName = key.split(":")[1];
-          if (storedEmoteName == reactName) {
-            if (value.fetched == true && userCount != 0) {
-              console.log(key);
-              console.log(value);
-              const userList = Object.keys(value.users);
-              const foundCount = userList.length;
-              if (foundCount == reactMenuCount) {
-                //match
-                copyToClipboard(userList.join(" "));
-                BdApi.alert(
-                  "Copied to clipboard!",
-                  `There are: ${reactMenuCount} reactions. Found: ${foundCount}.`
-                );
-              } else {
-                BdApi.alert(
-                  "FAIL!",
-                  `There are: ${reactMenuCount} reactions. Found: ${foundCount}.`
-                );
-              }
+          const reactionMsgId = key.split(":")[0];
+
+          if (
+            storedEmoteName == reactName &&
+            value.fetched == true &&
+            userCount != 0 &&
+            reactionMsgId == currentMsgId
+          ) {
+            console.log(key);
+            console.log(value);
+            const userList = Object.keys(value.users);
+            const foundCount = userList.length;
+            if (foundCount == reactMenuCount) {
+              let editedList = userList.map(id => `<@${id}>`);
+              //match
+              copyToClipboard(editedList.join(" "));
+              BdApi.alert(
+                "Copied to clipboard!",
+                `There are: ${reactMenuCount} reactions. Found: ${foundCount}.`
+              );
+            } else {
+              BdApi.alert(
+                "FAIL!",
+                `There are: ${reactMenuCount} reactions. Found: ${foundCount}.`
+              );
             }
           }
         }
-
-        // OBSERVER PART
-
-        function NestedParent(element, selector) {
-          return element.closest(selector);
-        }
-
-        // Callback function to handle the mutations
-        function handleMutations(mutations) {          
-          mutations.forEach(function (mutation) {
-            mutation.addedNodes.forEach(function (node) {
-              if (node.matches && node.matches("[class^=chatContent]")) {
-                onRightClick(node);
-              } else {
-                //console.log("Ignored right click")
-              }
-            });
-          });
-        }
-
-        function onRightClick(node) {          
-          console.log('Adding right click listener. context:', this)
-          // The element with the selector '[class^=chatContent]' has been added
-          console.log(
-            "Element with selector '[class^=chatContent]' added:",
-            node
-          );
-          node.addEventListener("contextmenu", function (event) {
-            
-            var targetElement = event.target;
-            var parentElement = NestedParent(
-              targetElement,
-              "[id^=chat-messages]"
-            );
-            if (parentElement != null) {
-              var parentElemParts = parentElement.id.split("-");
-              var msgId = parentElemParts[parentElemParts.length - 1];
-              currentMsgId = msgId;
-              console.log("Right clicked! Id is ", msgId);
-            }
-          });
-        }
-
-        // Create a new mutation observer
-        observer = new MutationObserver(handleMutations);
-
-        var chatContentSelector = document.querySelector(
-          "[class^=chatContent]"
-        );
-        if (chatContentSelector != null) {
-          onRightClick(chatContentSelector);
-        }
-
-        // Start observing the document body for mutations
-        observer.observe(document.body, { childList: true, subtree: true });
-
-        // list = [];
-        // let children = document.querySelector(
-        //   `${reactorsSelector} > div`
-        // ).children;
-
-        // function atTop() {
-        //   console.log("atTop start");
-        //   var elem = document.querySelector(reactorsSelector);
-        //   var children = document.querySelector(
-        //     `${reactorsSelector} > div`
-        //   ).children;
-        //   if (elem.scrollTop == 0) {
-        //     console.log("at the top");
-        //     next(list);
-        //   } else {
-        //     console.log("Scrolling up");
-        //     children[0].scrollIntoView();
-        //     setTimeout(atTop, 1000);
-        //   }
-        // }
-
-        // atTop();
-
-        // function next(list) {
-        //   console.log("aaaaa");
-
-        //   console.log("starting thing");
-        //   for (let i = 0; i < children.length; i++) {
-        //     if (children[i].children.length != 0) {
-        //       while (
-        //         typeof children[i].querySelector(
-        //           "strong > div > span[class*=username]"
-        //         ) === "null"
-        //       ) {
-        //         //pass
-        //       }
-        //       //console.log("[1] added children:")
-        //       //console.log(children[i])
-        //       if (children[i].querySelector("span[class*=spinner]") != null) {
-        //         console.log("spinner");
-        //       } else {
-        //         let pfpUrl = children[i]
-        //           .querySelector("div > div > svg > foreignObject > div > img")
-        //           .src.split("/");
-        //         let pfpType = pfpUrl[3];
-        //         if (pfpType == "avatars") {
-        //           var pingerTxt = `<@${pfpUrl[4]}>`;
-        //         } else {
-        //           console.log("No pfp");
-        //           console.log(children[i]);
-        //           let name = children[i].querySelector(
-        //             "strong > div > span[class*=username]"
-        //           ).innerHTML;
-
-        //           let discriminator = "";
-        //           var selector = children[i].querySelector(
-        //             "strong > div > span[class*=discriminator]"
-        //           );
-        //           if (selector != null) {
-        //             discriminator = selector.innerHTML;
-        //           }
-        //           var pingerTxt = `@${name}${discriminator}`;
-        //         }
-        //         list.push(pingerTxt);
-        //         console.log(`Added ${pingerTxt}`);
-        //       }
-        //     }
-        //   }
-
-        //   console.log("starting thing2");
-        //   var lastElem = null;
-
-        //   var mutationObserver = new MutationObserver(function (mutations) {
-        //     /*creates observer*/
-        //     console.log("Mutation!");
-        //     for (let i = 0; i < mutations.length; i++) {
-        //       if (mutations[i].addedNodes.length != 0) {
-        //         // console.log('Mutation found:')
-        //         // console.log(mutations[i].addedNodes[0])
-
-        //         while (
-        //           typeof mutations[i].addedNodes[0].querySelector(
-        //             "strong > div > span[class*=username]"
-        //           ) === "null"
-        //         ) {
-        //           //pass
-        //         }
-        //         //console.log("[2] added nodes:")
-        //         //console.log(mutations[i].addedNodes[0])
-        //         if (
-        //           document.querySelector(`${reactorsSelector} > div > span`) !=
-        //           null
-        //         ) {
-        //           console.log("spinner");
-        //         } else {
-        //           let pfpUrl = mutations[i].addedNodes[0]
-        //             .querySelector(
-        //               "div > div > svg > foreignObject > div > img"
-        //             )
-        //             .src.split("/");
-        //           let pfpType = pfpUrl[3];
-        //           if (pfpType == "avatars") {
-        //             var pingerTxt = `<@${pfpUrl[4]}>`;
-        //           } else {
-        //             console.log("No pfp");
-        //             console.log(mutations[i]);
-        //             console.log(
-        //               mutations[i].addedNodes[0].querySelector(
-        //                 "strong > div > span[class*=discriminator]"
-        //               )
-        //             );
-        //             let name = mutations[i].addedNodes[0].querySelector(
-        //               "strong > div > span[class*=username]"
-        //             ).innerHTML;
-        //             let discriminator = "";
-        //             var selector = mutations[i].addedNodes[0].querySelector(
-        //               "strong > div > span[class*=discriminator]"
-        //             );
-        //             if (selector != null) {
-        //               discriminator = selector.innerHTML;
-        //             }
-        //             var pingerTxt = `@${name}${discriminator}`;
-        //           }
-        //           list.push(pingerTxt);
-        //           console.log(`Added ${pingerTxt}`);
-        //         }
-        //       }
-        //     }
-
-        //     if (mutations[mutations.length - 1].addedNodes[0] != lastElem) {
-        //       if (mutations[mutations.length - 1].addedNodes.length != 0) {
-        //         var lastElem = mutations[mutations.length - 1].addedNodes[0];
-        //         console.log("scrolling");
-        //         lastElem.scrollIntoView();
-        //       } else {
-        //         console.log("end?");
-        //         console.log(list.join(" "));
-        //         console.log(`length: ${list.length}`);
-        //       }
-        //     }
-        //     var elem = document.querySelector(reactorsSelector);
-        //     if (elem.scrollHeight - elem.scrollTop === elem.clientHeight) {
-        //       isEnd();
-        //     } else {
-        //       console.log(
-        //         `scroll height: ${
-        //           elem.scrollHeight - elem.scrollTop
-        //         } max height: ${elem.clientHeight}`
-        //       );
-        //     }
-        //   });
-
-        //   mutationObserver.observe(
-        //     document.querySelector(`${reactorsSelector} > div`),
-        //     {
-        //       childList: true,
-        //     }
-        //   );
-
-        //   var elem = document.querySelector(reactorsSelector);
-        //   if (elem.scrollHeight - elem.scrollTop === elem.clientHeight) {
-        //     isEnd();
-        //   } else {
-        //     console.log(
-        //       `scroll height: ${
-        //         elem.scrollHeight - elem.scrollTop
-        //       } max height: ${elem.clientHeight}`
-        //     );
-        //   }
-
-        //   let lastChild = children[children.length - 1];
-        //   let name = lastChild.querySelector(
-        //     "strong > div > span[class*=username]"
-        //   ).innerHTML;
-        //   let discriminator = "";
-        //   var selector = lastChild.querySelector(
-        //     "strong > div > span[class*=discriminator]"
-        //   );
-        //   if (selector != null) {
-        //     discriminator = selector.innerHTML;
-        //   }
-        //   lastChild.scrollIntoView();
-        //   console.log(`Scrolled to ${name}${discriminator}`);
-        //   setTimeout(isEnd, 3000);
-        //   function isEnd() {
-        //     console.log("isEnd start");
-        //     var elem = document.querySelector(reactorsSelector);
-        //     if (elem.scrollHeight - elem.scrollTop === elem.clientHeight) {
-        //       if (
-        //         document.querySelector(`${reactorsSelector} > div > span`) ==
-        //         null
-        //       ) {
-        //         console.log("end");
-        //         console.log(list.join(" "));
-        //         console.log(`length: ${list.length}`);
-        //         reactMenuCount = document.querySelector(
-        //           `${scrollerSelector} > [class^=reactionSelected] > [class*=text]`
-        //         ).innerHTML;
-        //         if (parseInt(reactMenuCount) == list.length) {
-        //           console.log(`MATCH!`);
-        //         }
-        //         console.log(`Menu: ${reactMenuCount} - List: ${list.length}`);
-        //         final(reactMenuCount, list.length, list);
-        //         mutationObserver.disconnect();
-        //       } else {
-        //         console.log("loading");
-        //         setTimeout(isEnd, 1000);
-        //       }
-        //     } else {
-        //       setTimeout(isEnd, 1000);
-        //     }
-        //   }
-        // }
-
-        // function final(reactMenuCount, found, list) {
-        //   copyToClipboard(list.join(" "));
-        //   var success_msg = "";
-        //   if (reactMenuCount == found) {
-        //     var success_msg = "SUCCESS!";
-        //   } else {
-        //     var success_msg = "FAIL! Try it again.";
-        //   }
-        //   BdApi.alert(
-        //     "Copied to clipboard!",
-        //     `There are: ${reactMenuCount} reactions. Found: ${found}.\n${success_msg}`
-        //   );
-        // }
       }
+
+      // OBSERVER PART
+
+      // Create a new mutation observer
+
+      var chatContentSelector = document.querySelector("[class^=chatContent]");
+      if (chatContentSelector != null) {
+        addRightClickListener(chatContentSelector);
+      }
+
+      // Start observing the document body for mutations
+      console.log("Observing");
+      observer.observe(document.body, { childList: true, subtree: true });
 
       function copyReactions() {
         let sidebar = document.querySelector(scrollerSelector);
@@ -378,8 +159,20 @@ module.exports = (meta) => {
       copyReactions();
     },
     stop: () => {
+      console.log("CopyReactions stopping...");
       running = false;
+      console.log("Disconnecting observer...", observer);
       observer.disconnect();
+
+      console.log("CopyReactions: Removing listeners");
+      // Remove all event listeners
+
+      listeners.forEach((event) => {
+        document.querySelector("[class^=chatContent]").removeEventListener(event.eventType, event.listener);
+      });
+
+      // Clear the event listeners array
+      listeners.length = 0;
     },
   };
 };
